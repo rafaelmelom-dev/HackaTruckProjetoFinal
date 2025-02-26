7e8510575cfae78b856b5e9d8c7b929b947decfa
Rafael Moreira de Melo <132792028+rafaelmelom-dev@users.noreply.github.com>
2025-02-26 14:02:15 -0300

    Add files via upload

---
+++ ProjetoFinal/ProjetoFinal/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ProjetoFinal
+//
+//  Created by Turma02-23 on 25/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            TabView {
+                Home()
+                    .tabItem {
+                        Label("Início", systemImage: "house")
+                    }
+                
+                Busca()
+                    .tabItem {
+                        Label("Busca",
+                              systemImage: "magnifyingglass")
+                    }
+                
+                Historico()
+                    .tabItem {
+                        Label("Histórico",
+                              systemImage: "list.bullet.rectangle.portrait")
+                    }
+                
+                Perfil()
+                    .tabItem {
+                        Label("Perfil",
+                              systemImage: "person.circle.fill")
+                    }
+            
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjetoFinal/ProjetoFinal/ProjetoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoFinalApp.swift
+//  ProjetoFinal
+//
+//  Created by Turma02-23 on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
